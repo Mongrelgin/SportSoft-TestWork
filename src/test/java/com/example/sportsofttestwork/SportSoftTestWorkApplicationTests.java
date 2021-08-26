a98bb726a2912bd758829146caeebcd5a8c62bb4
Ivan Koloyanov <ikoloyanov@intetech.ru>
2021-08-26 21:24:07 +0300

    Initial Commit

---
+++ src/test/java/com/example/sportsofttestwork/SportSoftTestWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sportsofttestwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportSoftTestWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
